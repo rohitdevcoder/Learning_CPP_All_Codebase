fd5508cee523437847b142de720eea51e986d222
rohitdevcoder <rohitdevlink98@gmail.com>
2025-06-19 12:05:35 +0530

    First commit

---
+++ Day-1/Conditional-Statements/conditional.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace  std;
+
+//command : g++ conditional.cpp -o conditional.exe;.\conditional.exe
+
+int main(){
+
+    cout << "Hello" <<endl;
+
+    return 0;
+}
---
+++ Day-1/datatype/datatype.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+// command : g++ datatype.cpp -o datatype.exe;.\datatype.exe
+
+int main(){
+    // char alphabet = 'B';
+    // int num = alphabet;
+    // cout << num <<endl;
+
+    // double price = 100.990;
+    // int  newPrice = (int)price;
+    // cout<< newPrice << endl;
+
+    // int age;
+    // cout <<  "Enter Your Age : ";
+    // cin >> age;
+    // cout << "Your age is : "<<age <<endl;
+
+    // cout << "Type casting division value : " << (11/(double)5)<< endl;
+
+    // cout << (2<6) << "\n";
+    // cout << (2==6) << "\n";
+
+    // cout << !(2==6) << "\n";
+
+    // int a = 10;
+    // int b= a++;
+    // cout<<  b << endl;
+    // cout<<  a << endl;
+    int a = 10;
+    int b= ++a;
+    cout<<  b << endl;
+    cout<<  a << endl;
+
+    return 0;
+}
