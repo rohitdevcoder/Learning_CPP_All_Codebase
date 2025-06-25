a0972e07c47022cf1147c954787365154abb4d65
rohitdevcoder <rohitdevlink98@gmail.com>
2025-06-25 13:41:00 +0530

    Day-5 and array assignment added

--- Day-4/Assignment/assignment.cpp
+++ Day-4/Assignment/assignment.cpp
@@ -1,7 +1,78 @@
 #include <iostream>
 using namespace std;
 
+// int productArray(int arr[],int sz){
+//     int product=1;
+//     for(int i = 0;i<sz;i++){
+//      product *=arr[i];
+//     }
+//     return product;
+// }
+
+// int sumArray(int arr[],int sz){
+//     int sum = 0;
+//     for(int i = 0; i<sz;i++){
+//         sum +=arr[i];
+//     }
+//     return sum;
+// }
+
 int main(){
-    cout << "Hello World";
-    return  0;
+    // ----- calculate the product------
+//   int nums[] = {1,2,3};
+//   int size = sizeof(nums)/sizeof(int);
+//   cout << "The Product is : "<<productArray(nums,size)<<endl;
+
+// int nums[] = {1,2,3,4};
+// int size = sizeof(nums)/sizeof(int);
+// cout << " Sum of all numbers in array : "<<sumArray(nums,size)<<endl;
+
+// -------swap the minimum and max in array---------
+// int nums[] = {21,65,4,-98,23,74,45};
+// int size = sizeof(nums)/sizeof(int);
+// int minNum=INT8_MAX;
+// int maxNum = INT8_MIN;
+// for(int i = 0; i<size;i++){
+//     minNum = min(nums[i],minNum);
+//     maxNum = max(nums[i],maxNum);
+
+// }
+// cout << "The minimum and maximum number is : "<<minNum <<" & "<<maxNum << '\n';
+// swap(minNum,maxNum);
+// cout << "After swaping min and max term is : "<<'\n';
+// cout << "Mex Term : "<< maxNum << '\n';
+// cout <<  "Min Term : "<<minNum << endl; 
+
+//------print the unique values in an array-----------
+// int  nums[] = {1,1,2,2,3};
+// int size = sizeof(nums)/sizeof(int);
+// int unique=0;
+
+// for(int i =0;i<size;i++){
+//     unique^=nums[i];
+// }
+// cout<< "The unique element is : "<<unique;
+
+// -------print the intersectionn of two array-----------
+int arrayOne[]={1,3,7,9,8,5,6,4};
+int sizeOne = sizeof(arrayOne)/sizeof(int);
+int arrayTwo[]={1,2,3,4,5,12,7,8,};
+int sizeTwo = sizeof(arrayTwo)/sizeof(int);
+int inerArray[100]={false};
+
+cout << "Intersection : ";
+for(int i =0;i<sizeOne;i++){
+    for(int j=0;j<sizeTwo;j++){
+        if (arrayOne[i]==arrayTwo[j] && !inerArray[j])
+        {
+        cout<<arrayOne[i]<<" ";
+        inerArray[j]=true;
+        break;
+        }
+        
+    }
+}
+
+
+  return  0;
 }
---
+++ Day-5/Vector/vector.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main(){
+//   cout << "Helllo World";
+// vector<int>vec={1,3,4};
+// cout <<vec[2];
+// for(int i : vec){
+//     cout << i<<endl;
+// }
+
+// vector<char>charecter = {'a','b','c','d','e'};
+// for(char i : charecter){
+// cout << i << " ";
+// }
+
+//-------vectror Functions-----
+// vector<int>vec={1,3,4};
+// cout << "Size of the vector : "<<vec.size();
+// vec.push_back(6);
+// vec.pop_back();
+
+// for(int i : vec){
+// cout << i << " ";
+// }
+// cout << "Front Value : "<< vec.front()<< endl;
+// cout << "Back Value : "<< vec.back()<< endl;
+// cout << "1th index Value : "<< vec.at(1)<< endl;
+
+
+    return 0;
+}
